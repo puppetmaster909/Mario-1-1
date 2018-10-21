1e652c228598a6d417ac0c41c41f8df9bff07ed7
Puppetmaster909 <pringthomas4@gmail.com>
2018-10-21 05:26:42 -0400

    Finished Tier 1

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+
+    public bool bounds;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        transform.position = new Vector3(player.position.x + offset.x, 2, -10);
+
+        if (bounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+        }
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    private bool facingRight = true;
+
+    public float speed;
+    public float jumpForce;
+
+    //ground check
+    private bool isOnGround;
+    public Transform groundcheck;
+    public float checkRadius;
+    public LayerMask allGround;
+
+    //Audio Clips
+    public AudioClip jumpSmallSound;
+    public AudioClip jumpLargeSound;
+    public AudioClip coinSound;
+    public AudioClip deathSound;
+    public AudioClip goombaDeathSound;
+    public AudioClip endOfLevelSound;
+
+
+    // private float jumpTimeCounter;
+    //public float jumpTime;
+    //private bool isJumping;
+
+    //audio stuff
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Awake()
+    {
+
+       // source = GetComponent<AudioSource>();
+
+    }
+
+    private void Update(){
+       
+    }
+
+    
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 easeVelocity = rb2d.velocity;
+        easeVelocity.x *= 0.75f;
+        easeVelocity.y = rb2d.velocity.y;
+        easeVelocity.z = 0.0f;
+    
+        //Fake friction
+        if (isOnGround)
+        {
+            rb2d.velocity = easeVelocity;
+        }
+
+        // Jumping
+        if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && isOnGround == true)
+        {
+            anim.SetBool("ground", false);
+            rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+
+        //Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        // rb2d.AddForce(movement * speed);
+
+        rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
+
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+        anim.SetBool("ground", isOnGround);
+
+        //Debug.Log(isOnGround);
+
+
+
+        //stuff I added to flip my character
+        if(facingRight == false && moveHorizontal > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && moveHorizontal < 0)
+        {
+            Flip();
+        }
+
+        //Section for Animations
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetTrigger("jump");
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+    //Pickup Method
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            GetComponent<AudioSource>().PlayOneShot(coinSound);
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground" && isOnGround)
+        {
+
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+               //rb2d.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+               rb2d.velocity = Vector2.up * jumpForce;
+                
+                
+                // Audio stuff
+              
+                
+                
+            }
+        }
+    }
+}
